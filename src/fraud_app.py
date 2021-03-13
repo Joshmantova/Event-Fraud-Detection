6b596dc381391ad978f25e610dc88cc9691afccf
Josh Mantovani <josh.mantova@gmail.com>
2021-03-13 15:32:29 -0700

    Created fully functional streamlit app

--- src/fraud_app.py
+++ src/fraud_app.py
@@ -1,6 +1,31 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
+from PIL import Image
 from nlp_model import spacy_tokenizer
 from clean_data_and_predict import Data
 
+data = Data()
+raw_data = data.raw_data
+clean_data = data.clean()
+prediction = data.predict(clean_data)
+
+if prediction < .25:
+    ordinal_prediction = 'Low'
+elif prediction > .25 and prediction < .70:
+    ordinal_prediction = 'Moderate'
+elif prediction > .70:
+    ordinal_prediction = 'High'
+
+event_name = raw_data['name'][0]
+org_name = raw_data['org_name'][0]
+
+st.write(f"Raw data")
+st.write(raw_data)
+st.write(f"Organization name: {org_name}")
+
+img = Image.open('imgs/Final_Prediction_Pipeline.png')
+st.image(img)
+
+st.write(f"Fraud risk: {ordinal_prediction}")
+st.write(f"Likelihood of fraud: {prediction}")
